f166f713f3a31585b96443a4e475efa5d1099063
Lewis Tansley <officialinsanityproject@gmail.com>
2023-03-13 18:58:22 +0000

    StartPage v1.0.0

---
+++ bookmarks.js
@@ -0,0 +1,50 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com", img: "images/mail.png"},
+      { name: "Keep", url: "https://keep.google.com", img: "images/keep.png" },
+      { name: "Fast", url: "https://fast.com/", img: "images/fast.png" },
+      { name: "Portfolio", url: "https://lewistansley.github.io/" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com", img: "images/yt.png"  },
+      { name: "Netflix", url: "https://netflix.com", img: "images/netflix.png" },
+      { 
+        name: "Amazon Prime", 
+        url: "https://www.amazon.com/Amazon-Video", 
+        img: "images/prime.png"},
+    ],
+  },
+
+  {
+    title: "Social",
+    links: [
+      { name: "Discord", url: "https://discord.com/app", img: "images/discord.png" },
+      { name: "Twitter", url: "https://twitter.com", img: "images/twitter.png" },
+      { name: "Facebook", url: "https://facebook.com", img: "images/facebook.png" },
+    ],
+  },
+  {
+    title: "Programming",
+    links: [
+      { name: "GitHub", url: "https://github.com", img: "images/git.png" },
+      { name: "StackOverflow", url: "https://stackoverflow.com", img: "images/stack.png" },
+      { name: "Trello", url: "https://trello.com/tansleylewis/boards", img: "images/trello.png" },
+    ],
+  },
+  {
+    title: "Linux",
+    links: [
+      { name: "Arch Wiki", url: "https://wiki.archlinux.org/" },
+      { name: "ArcoLinux", url: "https://arcolinux.com/" },
+      { name: "ProtonDB", url: "https://www.protondb.com/" },
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+
+    ],
+  },
+];
